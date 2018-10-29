ff8f8ac381742f70b4bab3c23b72f88c1d5ad76c
Talisson Ferreira <talissonf@gmail.com>
2018-10-29 01:06:16 -0300

    App main

---
+++ index.js
@@ -0,0 +1,37 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const routes = require('./lib/routes')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+process.on('uncaughtException', (err) => {
+  // fatal error
+  // log to file before exit
+  let msg = '\n[' + new Date().toString() + ']\n'
+  if (err) {
+    if (err.hasOwnProperty('stack')) {
+      msg += err.stack
+    } else if (err.hasOwnProperty('message')) {
+      msg += err.message
+    } else {
+      msg += err.toString()
+    }
+    msg += '\n'
+  }
+
+  let fs = require('fs')
+  fs.appendFile('_stderr', msg, () => {
+    process.exit(1)
+  })
+})
+
+app.get('/redirect', routes.autentication.oauth)
+app.get('/callback', routes.autentication.setToken)
+app.get('/keys', routes.autentication.getKey)
+app.post('/customers', routes.customers.new)
+app.post('/payments/list', routes.payments.list)
+
+app.listen(port)
