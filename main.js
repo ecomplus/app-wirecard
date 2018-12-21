b949bd1906dbc7c21d6e75b7c0f2812ba2cf4816
Talisson Ferreira <talissonf@gmail.com>
2018-12-21 15:13:49 -0200

    fixes

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 9090
 
 require('./bin/uncaughtException')
 
