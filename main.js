8aedc538abe2893057bee14e405ce052064f12a5
Talisson Ferreira <talissonf@gmail.com>
2019-01-03 17:14:38 -0200

    Revert to es5

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 
 'use strict'
-import bodyParser from 'body-parser'
-import express from 'express'
-import router from './lib/routes'
+const bodyParser = require('body-parser')
+const express = require('express')
+const router = require('./lib/routes')
 
 const app = express()
 const port = process.env.PORT || 9090
+
+require('./bin/uncaughtException')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
