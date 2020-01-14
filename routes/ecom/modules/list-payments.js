fe662384fa0365c938b6f59df96e692cf3b3f430
Talisson Ferreira <talissonf@gmail.com>
2020-01-13 23:24:46 -0300

    chore(list-payments): start using database.authentications

--- routes/ecom/modules/list-payments.js
+++ routes/ecom/modules/list-payments.js
@@ -1,124 +1,139 @@
 'use strict'
 const logger = require('console-files')
-const { getWirecardAuth } = require('./../../../lib/Api/Api')
+const { authentications } = require('./../../../lib/database')
 
 module.exports = () => {
   return (req, res) => {
     const { params, application } = req.body
     const storeId = req.get('x-store-id')
-    //  
-    getWirecardAuth(storeId).then(() => {
-      // parse params from body
-      const hiddenData = application.hidden_data || {}
 
-      // load application default config
-      let { payment_options, sort } = require('./../../../lib/payment-default')
+    authentications.get(storeId)
 
-      // array to merge config
-      let configMerged = []
+      .then(() => {
+        // parse params from body
+        const hiddenData = application.hidden_data || {}
 
-      // empty payload
-      let response = {
-        payment_gateways: []
-      }
-      // merge application default config with
-      // configuration sent at application.hidden_data
-      payment_options.forEach(defaultOption => {
-        // if the application not has payments config setted up, uses default.
-        if (!application.hasOwnProperty('hidden_data') || !hiddenData.hasOwnProperty('payment_options')) {
-          configMerged.push(defaultOption)
-        } else {
-          // Checks if default payment option is set in application.hidden_data
-          const applicationConfiguration = hiddenData.payment_options.find(applicationOption => applicationOption.type === defaultOption.type)
+        // load application default config
+        let { payment_options, sort } = require('./../../../lib/payment-default')
 
-          if (applicationConfiguration) {
-            // check if payment options is enabled to list at list_payments
-            if (applicationConfiguration.enabled === true) {
-              configMerged.push({
-                ...defaultOption,
-                ...applicationConfiguration
-              })
-            }
-          } else {
-            // uses payment_option default if option is not setted up at hiddenData.payment_options
+        // array to merge config
+        let configMerged = []
+
+        // empty payload
+        let response = {
+          payment_gateways: []
+        }
+        // merge application default config with
+        // configuration sent at application.hidden_data
+        payment_options.forEach(defaultOption => {
+          // if the application not has payments config setted up, uses default.
+          if (!application.hasOwnProperty('hidden_data') || !hiddenData.hasOwnProperty('payment_options')) {
             configMerged.push(defaultOption)
-          }
-        }
-      })
+          } else {
+            // Checks if default payment option is set in application.hidden_data
+            const applicationConfiguration = hiddenData.payment_options.find(applicationOption => applicationOption.type === defaultOption.type)
 
-      if (params && params.hasOwnProperty('items') && params.hasOwnProperty('amount')) {
-        // create payment option list for list_payment
-        // with merged configuration
-        configMerged.forEach(config => {
-          let paymentGateways = {}
-          paymentGateways.discount = listPaymentOptions.discount(config)
-          paymentGateways.icon = listPaymentOptions.icon(config)
-          paymentGateways.intermediator = listPaymentOptions.intermediator(config)
-          paymentGateways.label = listPaymentOptions.label(config)
-          paymentGateways.payment_method = listPaymentOptions.payment_method(config)
-          paymentGateways.payment_url = listPaymentOptions.payment_url(config)
-          paymentGateways.type = listPaymentOptions.type(config)
-          if ((config.type === 'credit_card')) {
-            paymentGateways.js_client = listPaymentOptions.js_client(config, hiddenData)
-            paymentGateways.installment_options = listPaymentOptions.installment_options(config, params)
-            paymentGateways.card_companies = config.card_companies
-          }
-
-          response.payment_gateways.push(paymentGateways)
-        })
-      }
-
-      // discount_options
-      configMerged.forEach(config => {
-        if (config.type === 'banking_billet' && config.hasOwnProperty('discount')) {
-          if (config.discount.value > 0) {
-            response.discount_options = {
-              label: config.name,
-              type: config.discount.type,
-              value: config.discount.value
+            if (applicationConfiguration) {
+              // check if payment options is enabled to list at list_payments
+              if (applicationConfiguration.enabled === true) {
+                configMerged.push({
+                  ...defaultOption,
+                  ...applicationConfiguration
+                })
+              }
+            } else {
+              // uses payment_option default if option is not setted up at hiddenData.payment_options
+              configMerged.push(defaultOption)
             }
           }
-        }
-      })
-
-      // discount_option
-      if (application.hasOwnProperty('hidden_data') && application.hidden_data.hasOwnProperty('discount_option')) {
-        const discountOption = application.hidden_data.discount_option || {}
-        response.discount_option = {
-          min_amount: discountOption.min_amount,
-          label: discountOption.label,
-          type: discountOption.type,
-          value: discountOption.value
-        }
-      }
-
-      // installments_option
-      if (application.hasOwnProperty('hidden_data') && application.hidden_data.hasOwnProperty('installments_option')) {
-        const installmentOptions = application.hidden_data.installments_option || {}
-        response.installments_option = {
-          min_installment: installmentOptions.min_installment,
-          max_number: installmentOptions.max_number,
-          monthly_interest: installmentOptions.monthly_interest
-        }
-      }
-
-      // sort config
-      if (application.hasOwnProperty('hidden_data') && hiddenData.hasOwnProperty('sort')) {
-        sort = [...hiddenData.sort, ...sort]
-      }
-
-      const sortFunc = (a, b) => sort.indexOf(a.payment_method.code) - sort.indexOf(b.payment_method.code)
-      response.payment_gateways.sort(sortFunc)
-
-      // response
-      return res.send(response)
-    })
-      .catch(err => {
-        logger.error('LIST_PAYMENTS_ERR', err)
-        return res.status(401).send({
-          error: 'LIST_PAYMENTS_ERR',
-          message: 'Unexpected Error Try Later'
         })
+
+        if (params && params.hasOwnProperty('items') && params.hasOwnProperty('amount')) {
+          // create payment option list for list_payment
+          // with merged configuration
+          configMerged.forEach(config => {
+            let paymentGateways = {}
+            paymentGateways.discount = listPaymentOptions.discount(config)
+            paymentGateways.icon = listPaymentOptions.icon(config)
+            paymentGateways.intermediator = listPaymentOptions.intermediator(config)
+            paymentGateways.label = listPaymentOptions.label(config)
+            paymentGateways.payment_method = listPaymentOptions.payment_method(config)
+            paymentGateways.payment_url = listPaymentOptions.payment_url(config)
+            paymentGateways.type = listPaymentOptions.type(config)
+            if ((config.type === 'credit_card')) {
+              paymentGateways.js_client = listPaymentOptions.js_client(config, hiddenData)
+              paymentGateways.installment_options = listPaymentOptions.installment_options(config, params)
+              paymentGateways.card_companies = config.card_companies
+            }
+
+            response.payment_gateways.push(paymentGateways)
+          })
+        }
+
+        // discount_options
+        configMerged.forEach(config => {
+          if (config.type === 'banking_billet' && config.hasOwnProperty('discount')) {
+            if (config.discount.value > 0) {
+              response.discount_options = {
+                label: config.name,
+                type: config.discount.type,
+                value: config.discount.value
+              }
+            }
+          }
+        })
+
+        // discount_option
+        if (application.hasOwnProperty('hidden_data') && application.hidden_data.hasOwnProperty('discount_option')) {
+          const discountOption = application.hidden_data.discount_option || {}
+          response.discount_option = {
+            min_amount: discountOption.min_amount,
+            label: discountOption.label,
+            type: discountOption.type,
+            value: discountOption.value
+          }
+        }
+
+        // installments_option
+        if (application.hasOwnProperty('hidden_data') && application.hidden_data.hasOwnProperty('installments_option')) {
+          const installmentOptions = application.hidden_data.installments_option || {}
+          response.installments_option = {
+            min_installment: installmentOptions.min_installment,
+            max_number: installmentOptions.max_number,
+            monthly_interest: installmentOptions.monthly_interest
+          }
+        }
+
+        // sort config
+        if (application.hasOwnProperty('hidden_data') && hiddenData.hasOwnProperty('sort')) {
+          sort = [...hiddenData.sort, ...sort]
+        }
+
+        const sortFunc = (a, b) => sort.indexOf(a.payment_method.code) - sort.indexOf(b.payment_method.code)
+        response.payment_gateways.sort(sortFunc)
+
+        // response
+        return res.send(response)
+      })
+      .catch(err => {
+        let response = {}
+        response.error = 'LIST_PAYMENTS_ERR'
+        let status = 400
+        switch (err.name) {
+          case 'WirecardAuthNotFound':
+            // return response with client error code
+            status = 401
+            response.message = `Authentication not found for store #${storeId}, please perform authorization flow again with wirecard.`
+            break
+          default:
+            status = 400
+            response.message = 'Unexpected Error Try Later'
+            logger.error(response.error, err)
+            break
+        }
+
+        res.status(status)
+        res.send(response)
       })
   }
 }
@@ -167,7 +182,6 @@ const listPaymentOptions = {
   },
   js_client: (config, hiddenData) => {
     if (config.type === 'credit_card') {
-      console.log(hiddenData)
       let pubk = hiddenData.public_key || ''
       let onloadFunction = 'window.wirecardHash=function(n){return MoipSdkJs.MoipCreditCard.setPubKey(' + JSON.stringify(pubk) + ').setCreditCard({number:n.number,cvc:n.cvc,expirationMonth:n.month,expirationYear:n.year}).hash()},window.wirecardBrand=function(n){return MoipValidator.cardType(n.number)};'
       return {
