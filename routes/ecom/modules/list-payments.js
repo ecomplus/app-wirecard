4aee090874fba7c774e538059672fd763c5519e5
Leonardo Matos <leomp120894@gmail.com>
2020-04-02 13:31:45 -0300

    fix(list-payments): fix `installments_option` with tax
    https://community.e-com.plus/t/wirecard-configuracoes/204/4

--- routes/ecom/modules/list-payments.js
+++ routes/ecom/modules/list-payments.js
@@ -132,10 +132,18 @@ module.exports = () => {
             }
 
             // installments_option
-            response.installments_option = {
-              min_installment: installments.min_installment,
-              max_number: installments.max_number,
-              monthly_interest: installments.monthly_interest
+            if (installments.max_number_w_tax > 0) {
+              response.installments_option = {
+                min_installment: installments.min_installment,
+                max_number: installments.max_number_w_tax,
+                monthly_interest: 0
+              }
+            } else {
+              response.installments_option = {
+                min_installment: installments.min_installment,
+                max_number: installments.max_number_w_tax,
+                monthly_interest: installments.tax_value
+              }
             }
           } else {
             // wirecard default installments
